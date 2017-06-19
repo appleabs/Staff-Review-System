c67441ab53e0bb6a1685551e792bfdf29e8c64fb
Jenny <myersj@aston.ac.uk>
2017-06-19 13:15:16 +0100

    controller methods and views

--- StaffReview/Models/Staff.cs
+++ StaffReview/Models/Staff.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace StaffReview.Models
 {
     public class Staff
     {
         public int ID { get; set; }
+
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Display(Name = "Job Title")]
         public string JobTitle { get; set; }
         public decimal Rating { get; set; }
         public string Review { get; set; }
