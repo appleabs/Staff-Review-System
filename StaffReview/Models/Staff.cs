d296900c6ed27f57cbe2876ae35d3bde9f8d4ec7
Jenny <myersj@aston.ac.uk>
2017-06-19 15:43:02 +0100

    adding validation

--- StaffReview/Models/Staff.cs
+++ StaffReview/Models/Staff.cs
@@ -6,13 +6,25 @@ namespace StaffReview.Models
     {
         public int ID { get; set; }
 
+        [StringLength(30, MinimumLength = 2)]
         [Display(Name = "First Name")]
+        [Required]
         public string FirstName { get; set; }
+
+        [StringLength(30, MinimumLength = 2)]
         [Display(Name = "Last Name")]
+        [Required]
         public string LastName { get; set; }
+
+        [StringLength(30, MinimumLength = 2)]
         [Display(Name = "Job Title")]
+        [Required]
         public string JobTitle { get; set; }
+
+        [Range(0, 10)]
         public decimal Rating { get; set; }
+
+        [Required]
         public string Review { get; set; }
     }
 }
