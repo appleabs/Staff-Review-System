a300a9cd2c9cef3166ef7d0a82a688361916dd9c
Jenny <myersj@aston.ac.uk>
2017-06-19 13:05:26 +0100

    SQL sever Local DB tut complete

---
+++ StaffReview/Models/SeedData.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace StaffReview.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new StaffReviewContext(
+                serviceProvider.GetRequiredService<DbContextOptions<StaffReviewContext>>()))
+            {
+                // Look for any movies.
+                if (context.Staff.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Staff.AddRange(
+                     new Staff
+
+                     {
+                         FirstName = "John",
+                         JobTitle = "Marketing manager",
+                         LastName = "Smith",
+                         Rating = 5,
+                         Review = "Works hard!" 
+                     },
+
+                     new Staff
+
+                     {
+                         FirstName = "Anna",
+                         JobTitle = "HR intern",
+                         LastName = "Johnson",                        
+                         Rating = 4,
+                         Review ="Tries hard but needs more commitment."
+                     },
+                       
+                     new Staff
+
+                       {
+                           FirstName = "Adam",
+                           JobTitle = "Coal Miner",
+                           LastName = "Jacobs",
+                           Rating = 7,
+                           Review = "Excellent skills!"
+                       }
+
+                ); 
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- StaffReview/Startup.cs
+++ StaffReview/Startup.cs
@@ -60,6 +60,8 @@ namespace StaffReview
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            SeedData.Initialize(app.ApplicationServices);
         }
     }
 }
